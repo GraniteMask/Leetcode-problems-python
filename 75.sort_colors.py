ab1acb219a8ea1c06fc487279ea7ee42155f7994
GraniteMask <ratnadeepastronomy@gmail.com>
2022-05-25 16:10:15 +0530

    75_sort_colors_brute_method

--- 75.sort_colors.py
+++ 75.sort_colors.py
@@ -1,4 +1,4 @@
-# Brute - Swapping approach
+# Optimized in terms of memory usage 
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
@@ -12,3 +12,29 @@ class Solution:
                     nums[i] = nums[j]
                     nums[j] = temp
                     print(nums)
+
+# Optimized in terms of Runtime
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l,r = 0, len(nums)-1
+        i=0
+        
+        def swap(i,j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+        
+        while i<=r:
+            if nums[i] == 0:
+                swap(l,i)
+                l +=1
+            elif nums[i] == 2:
+                swap(i,r)
+                r-=1
+                i-=1  #this is used so that i increment effect is cancel out as we don't want to increment i when we find nums[i] == 2
+            i += 1
+                
