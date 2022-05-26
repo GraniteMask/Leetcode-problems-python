ae2e84660a31dbb1f4ea85af23a850aa6a7b80e4
GraniteMask <ratnadeepastronomy@gmail.com>
2022-05-27 04:21:51 +0530

    169.majority_element

---
+++ 169.majority_element.py
@@ -0,0 +1,13 @@
+# Brute
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+             
+        for i in range(len(nums)):
+            count = 0
+            for j in range(i+1, len(nums)):
+                if nums[j] == nums[i]:
+                    count += 1
+            
+            if count == len(nums) // 2:
+                return nums[i]
