3122a9cc51c08d52e4441f3f3e1cee37ea01930a
GraniteMask <ratnadeepastronomy@gmail.com>
2022-05-27 04:43:48 +0530

    169.majority_element

--- 169.majority_element.py
+++ 169.majority_element.py
@@ -12,7 +12,7 @@ class Solution:
             if count == len(nums) // 2:
                 return nums[i]
 
-# More Efficient solution
+# More Efficient solution - Time complexity O(nlogn) and space complexity O(n) - Tim sort
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
@@ -20,3 +20,15 @@ class Solution:
         nums.sort()
             
         return nums[len(nums)//2]
+
+# Solution with O(1) space complexity       
+ 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+             
+        res, count = 0, 0
+
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
