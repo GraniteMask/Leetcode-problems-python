067eb09b1c479097678c280188e45a9cd083dc79
GraniteMask <ratnadeepastronomy@gmail.com>
2022-05-28 04:57:58 +0530

    128.longest consecutive sequence

---
+++ longest_consecutive_sequence.py
@@ -0,0 +1,18 @@
+# Optimized solution of O(n) time complexity and O(n) memory complexity
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        print(nums)
+        print(numSet)
+        longest = 0
+        
+        for n in nums:
+            if (n-1) not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+        
+        
