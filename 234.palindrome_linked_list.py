01b22baf14ac2475db32c8a2b02744bd2aaee0dd
GraniteMask <ratnadeepastronomy@gmail.com>
2022-05-30 03:54:47 +0530

    234.palindrome_linked_list optimized

--- 234.palindrome_linked_list.py
+++ 234.palindrome_linked_list.py
@@ -27,3 +27,40 @@ class Solution:
 
 
 # OPTIMIZED TECHNIQUE - O(n) time and O(1) space complexities
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        fast = head
+        slow = head
+        
+        # find middle(slow)
+        
+        while fast and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+            
+        
+        # Reversing the linked list
+        
+        prev = None
+        
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+            
+        # check palindrome
+        
+        left, right = head, prev
+        
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+            
+        return True
+            
