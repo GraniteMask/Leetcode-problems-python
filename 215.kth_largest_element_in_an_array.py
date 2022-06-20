4f5511c150c691bb7d30e9f627b3b446565a7244
GraniteMask <ratnadeepastronomy@gmail.com>
2022-06-20 10:07:33 +0530

    215.kth largest element in an array

--- 215.kth_largest_element_in_an_array.py
+++ 215.kth_largest_element_in_an_array.py
@@ -4,3 +4,25 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort()
         return nums[len(nums) - k]
+
+
+# Using Quick sort - O(klogn)
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+        
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            
+            nums[p], nums[r] = nums[r], nums[p]   # nums[r] = pivot
+            
+            if p > k: return quickSelect(l, p - 1)
+            elif p < k: return quickSelect(p + 1, r)
+            else: return nums[p]
+            
+        return quickSelect(0, len(nums) - 1)
