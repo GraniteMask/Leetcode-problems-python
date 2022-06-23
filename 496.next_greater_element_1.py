a5f90f19140bd007278d4285bf2b8b7486f89f94
GraniteMask <ratnadeepastronomy@gmail.com>
2022-06-24 01:27:32 +0530

    496.next greater element 1

--- 496.next_greater_element_1.py
+++ 496.next_greater_element_1.py
@@ -14,3 +14,21 @@ class Solution:
                        
         return output
                      
+# Optimized method - O(m+n) solution
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        nums1Idx = {n:i for i, n in enumerate(nums1)}
+        res = [-1]*len(nums1)
+        
+        stack = []
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums1Idx[val]
+                res[idx] = cur
+            if cur in nums1Idx:
+                stack.append(cur)
+        return res
