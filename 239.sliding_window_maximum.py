483ba0d3f2b22ca6e224f580dc7e0e04bea68c2e
GraniteMask <ratnadeepastronomy@gmail.com>
2022-06-26 04:16:27 +0530

    239 sliding window maximum

--- 239.sliding_window_maximum.py
+++ 239.sliding_window_maximum.py
@@ -13,3 +13,29 @@ class Solution:
             
         return largest
             
+# Optimized solution - O(n) time complexity
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+        output = []
+        q = collections.deque()  # stores index
+        l = r = 0
+        
+        while r < len(nums):
+            # pop smaller values from q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+            
+            # remove left value from window
+            if l > q[0]:
+                q.popleft()
+                
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+            
+        return output
+            
