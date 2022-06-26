9d20eee1df8b8741878cfe58c69aec1162086029
GraniteMask <ratnadeepastronomy@gmail.com>
2022-06-26 21:00:55 +0530

    901. Online Stock Span

---
+++ 901.online_stock_span.py
@@ -0,0 +1,27 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.q = collections.deque()  # pair: [price, span]
+
+    def next(self, price: int) -> int:
+        if len(self.q) == 0:
+            self.q.append([price, 1])
+            return self.q[-1][1]
+        else:
+            if self.q[-1][0] <= price:
+                temp = 1
+                while self.q and self.q[-1][0] <= price:
+                    p, span = self.q.pop()
+                    temp += span
+                self.q.append([price, temp])
+                
+                
+            else:
+                self.q.append([price, 1])
+            print(self.q)
+            return self.q[-1][1]
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
