25da52cf4d82465356e90734df065d7e6595908c
GraniteMask <ratnadeepastronomy@gmail.com>
2022-07-04 19:19:00 +0530

    242.valid anagram

---
+++ 242.valid_anagram.py
@@ -0,0 +1,17 @@
+# Brute Method - Memory Optimized Solution
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        else:
+            for i in s:
+                if i in t:
+                    s = s.replace(i, "", 1)
+                    t = t.replace(t[t.index(i)], "", 1)
+            
+            if len(s) == 0 and len(t) == 0:
+                return True
+            else:
+                return False
