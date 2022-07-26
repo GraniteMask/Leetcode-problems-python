80938fa8ae24fdb5fb6f43a6bf529b95165a90ac
GraniteMask <ratnadeepastronomy@gmail.com>
2022-07-27 03:02:54 +0530

    find median from data stream memory optimized sol

--- 295.find_median_from_data_stream.py
+++ 295.find_median_from_data_stream.py
@@ -1,3 +1,5 @@
+# Time Optimized solution
+
 class MedianFinder:
 
     def __init__(self):
@@ -33,3 +35,28 @@ class MedianFinder:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+
+
+
+
+# Memory Optimized solution - Beats 99.36% 
+
+class MedianFinder:
+
+    def __init__(self):
+        
+        self.array = []
+
+    def addNum(self, num: int) -> None:
+        
+        self.array.append(num)
+
+    def findMedian(self) -> float:
+        
+        self.array.sort()
+        l = len(self.array)
+
+        if l % 2 == 0:
+            return (self.array[(l-1) // 2] + self.array[((l-1) // 2) + 1]) / 2
+        elif l % 2 != 0:
+            return self.array[(l - 1) // 2]  
