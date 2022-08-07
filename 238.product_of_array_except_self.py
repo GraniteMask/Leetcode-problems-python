3607f4c6dffbde671054082aa32aea83610c867c
GraniteMask <ratnadeepastronomy@gmail.com>
2022-08-07 21:02:16 +0530

    238. Product of array except self

--- 238.product_of_array_except_self.py
+++ 238.product_of_array_except_self.py
@@ -13,3 +13,22 @@ class Solution:
             res.append(temp)
             
         return res
+
+# OPTIMIZED and O(1) extra space complexity solution
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        res = [1]*(len(nums))
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+            
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
